4cb4462f5c9c03d78a3a296f9bf4d9fb534d6667
ArtuomZhukov <Witcher5321@gmail.com>
2018-12-13 10:58:08 +0200

    Add fullscreen by doubleclick

--- 4MediaPlayer/4MediaPlayer/MainWindow.xaml.cs
+++ 4MediaPlayer/4MediaPlayer/MainWindow.xaml.cs
@@ -1,53 +1,56 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _4MediaPlayer
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        string MediaPath = "";
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        string path = "";
+
         private void LoadVideo_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog myDialog = new OpenFileDialog
             {
-                //Filter = /*"Видео(*.*)|*.*"*/"Видео(*.MP4;*.AVI;)|*.MP4;*.AVI",
                 CheckFileExists = true,
-                //Multiselect = true
             };
             if (myDialog.ShowDialog() == true)
             {
-                //GetLoadVideos(myDialog.FileNames);
-                path = myDialog.FileName;
+                MediaPath = myDialog.FileName;
             }
         }
 
         private void StartStopVideoButton_Click(object sender, RoutedEventArgs e)
         {
-            Uri source = new Uri(path);
+            Uri source = new Uri(MediaPath);
             VideoPlayer.Source = source;
             VideoPlayer.Play();
-            //string html = @"<iframe width=""560"" height=""315"" src=""http://www.youtube.com/watch?v=gToqBo7T-tI"" frameborder=""0"" allowfullscreen></iframe>";
-            //myWebView.NavigateToString(html);
+        }
+        private bool fullscreen = false;
+
+        private void VideoPlayer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (!fullscreen)
+                {
+                    WindowStyle = WindowStyle.None;
+                    WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    WindowStyle = WindowStyle.SingleBorderWindow;
+                    WindowState = WindowState.Normal;
+                }
+
+                fullscreen = !fullscreen;
+            }
         }
     }
 }
