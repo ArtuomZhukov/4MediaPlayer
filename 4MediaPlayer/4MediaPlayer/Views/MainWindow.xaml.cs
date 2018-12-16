981d55d041dd89f88d89ba56eccb2595103c6e59
ArtuomZhukov <Witcher5321@gmail.com>
2018-12-16 19:20:30 +0200

    Add new icons. Single & double click events

--- 4MediaPlayer/4MediaPlayer/Views/MainWindow.xaml.cs
+++ 4MediaPlayer/4MediaPlayer/Views/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -32,24 +36,49 @@ namespace _4MediaPlayer
             VideoPlayer.Source = source;
             VideoPlayer.Play();
         }
-        private bool fullscreen = false;
 
-        private void VideoPlayer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private async void VideoPlayer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.ClickCount == 2)
+            FrameworkElement ctrl = (FrameworkElement)sender;
+            ClickAttach.SetClicks(ctrl, e.ClickCount);
+            await Task.Delay(300);
+            DoClickStuff(ctrl);
+        }
+
+        private bool fullscreen = false;
+        private bool isPause = false;
+        private void DoClickStuff(FrameworkElement ctrl)
+        {
+            int clicks = ClickAttach.GetClicks(ctrl);
+            ClickAttach.SetClicks(ctrl, 0);
+            if (clicks > 0)
             {
-                if (!fullscreen)
+                if (clicks == 1)
                 {
-                    WindowStyle = WindowStyle.None;
-                    WindowState = WindowState.Maximized;
+                    if (isPause)
+                    {
+                        VideoPlayer.Play();
+                    }
+                    else
+                    {
+                        VideoPlayer.Pause();
+                    }
+                    isPause = !isPause;
                 }
                 else
                 {
-                    WindowStyle = WindowStyle.SingleBorderWindow;
-                    WindowState = WindowState.Normal;
+                    if (!fullscreen)
+                    {
+                        WindowStyle = WindowStyle.None;
+                        WindowState = WindowState.Maximized;
+                    }
+                    else
+                    {
+                        WindowStyle = WindowStyle.SingleBorderWindow;
+                        WindowState = WindowState.Normal;
+                    }
+                    fullscreen = !fullscreen;
                 }
-
-                fullscreen = !fullscreen;
             }
         }
     }
